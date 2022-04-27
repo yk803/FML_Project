7423c134c37f8d5bc739e3a33cdb4f1fc64e6e21
Yukai Yang <76096823+yk803@users.noreply.github.com>
2022-04-27 10:41:08 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 # This script can be used in Google Colab
 # To run the task on NYU HPC, please use the .slurm file instead.
-python pgd_adversarial_training_new.py checkpoint_soar_1 5
+python pgd_adversarial_train.py checkpoint_soar_1 5
