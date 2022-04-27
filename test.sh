44c89b0358e409ff11627ba03a22df393f481aa2
Yukai Yang <76096823+yk803@users.noreply.github.com>
2022-04-27 10:40:32 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # This script can be used in Google Colab
 # To run the task on NYU HPC, please use the .slurm file instead.
-python best_pgd_adversarial_training.pt checkpoint_soar_1
+python pgd_adversarial_test.py best_pgd_adversarial_training.pt checkpoint_soar_1
