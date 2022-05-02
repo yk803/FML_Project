7ba0764f4177e36e21249e42931db94ca6d8fbf2
Yukai Yang <76096823+yk803@users.noreply.github.com>
2022-05-02 16:20:40 -0400

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-# This script can be used in Google Colab
-# To run the task on NYU HPC, please use the .slurm file instead.
-python pgd_adversarial_test.py best_pgd_adversarial_training.pt checkpoint_soar_1
